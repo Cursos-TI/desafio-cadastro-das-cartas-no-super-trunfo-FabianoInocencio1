3afe6180c777f6083ebf94ede06f004e97922a45
FabianoInocencio1 <fa.inocencio28@gmail.com>
2025-01-15 14:11:42 +0000

    Adicionado calculos ao algoritimo desnidade demografica e pibpercapita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,14 +4,17 @@ int main() {
 
   printf("Desafio Super Trunfo - Países\n");
 
-int codigoDaCidade;
+int codigoDaCidade, pontoTuristico;
 char nome [50];
-float area, pib, populacao;
+float area, pib, populacao, DensidadePopulacional, PibPercapita;
 
 printf ("Adicione o código da cidade: \n");
 scanf ("%d", &codigoDaCidade);
 
-printf ("Adicione o nome de um ponto turístico: \n");
+printf("Informe o número de pontos turísitico:\n");
+scanf ("%d", &pontoTuristico);
+
+printf ("Adicione a cidade: \n");
  scanf ("%s", &nome);
 
 printf ("Digite a área territorial por KM da cidade: \n");
@@ -23,12 +26,18 @@ scanf ("%f", &pib);
 printf ("Digite a populacao:\n");
 scanf ("%f", &populacao);
 
+DensidadePopulacional = populacao / area;
+PibPercapita = pib / populacao * 100;
+
+
 printf("Código da cidade:  %d\n", codigoDaCidade);
-printf ("Nome dos pontos turísiticos:  %s \n", nome);
-printf ("Area Territorial: %.3f KM² \n", area);
+printf("Número de ponto turístico: %d\n", pontoTuristico);
+printf ("Cidade:  %s \n", nome);
+printf ("Area Territorial: %.2f KM² \n", area);
 printf ("Digite o PIB: %.2f  \n",pib);
 printf ("População: %.3f  \n ", populacao);
-
+printf ("Desindade Populacional: %.2f\n", DensidadePopulacional);
+printf ("Pib Percapita: %f %\n", PibPercapita);
 
 return 0;
 
