aff64eea25b539d51bb28a5885dae8fce5f66a8c
FabianoInocencio1 <fa.inocencio28@gmail.com>
2025-01-17 09:41:22 +0000

    atualizado cálculos e corrigido alguns erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,42 +2,77 @@
 
 int main() {
 
-  printf("Desafio Super Trunfo - Países\n");
+    printf("Desafio Super Trunfo - Países\n");
 
-int codigoDaCarta, pontoTuristico, populacao;
-char nome [50];
-float area, pib, DensidadePopulacional, PibPercapita;
+    int codigoDaCarta1, codigoDaCarta2, populacao1, populacao2;
+    char nome1[50], nome2[50];
+    float area1, area2, pib1, pib2, DensidadePopulacional1, DensidadePopulacional2, PibPercapita1, PibPercapita2;
 
-printf ("Adicione o código da carta: \n");
-scanf ("%d", &codigoDaCarta);
+    // Entrada para a primeira carta
+    printf("Adicione o código da carta 1: \n");
+    scanf("%d", &codigoDaCarta1);
 
-printf("Informe o número de pontos turísitico:\n");
-scanf ("%d", &pontoTuristico);
+    printf("Adicione o nome da cidade 1: \n");
+    scanf("%s", nome1);
 
-printf ("Adicione a cidade: \n");
- scanf ("%s", &nome);
+    printf("Digite a área territorial (em KM²) da cidade 1: \n");
+    scanf("%f", &area1);
 
-printf ("Digite a área territorial por KM da cidade: \n");
-scanf ("%f", &area);
+    printf("Digite o PIB da cidade 1: \n");
+    scanf("%f", &pib1);
 
-printf ("Digite o pib:\n");
-scanf ("%f", &pib);
+    printf("Digite a população da cidade 1: \n");
+    scanf("%d", &populacao1);
 
-printf ("Digite a populacao:\n");
-scanf ("%d", &populacao);
+    // Entrada para a segunda carta
+    printf("Adicione o código da carta 2: \n");
+    scanf("%d", &codigoDaCarta2);
 
-DensidadePopulacional = populacao / area;
-PibPercapita = pib / populacao * 100;
+    printf("Adicione o nome da cidade 2: \n");
+    scanf("%s", nome2);
+
+    printf("Digite a área territorial (em KM²) da cidade 2: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade 2: \n");
+    scanf("%f", &pib2);
+
+    printf("Digite a população da cidade 2: \n");
+    scanf("%d", &populacao2);
+
+    // Cálculos
+    DensidadePopulacional1 = populacao1 / area1;
+    PibPercapita1 = pib1 / populacao1;
+
+    DensidadePopulacional2 = populacao2 / area2;
+    PibPercapita2 = pib2 / populacao2;
+
+    // Cálculando o vencedor 
+
+    
 
 
-printf("Código da carta:  %d\n", codigoDaCarta);
-printf("Número de ponto turístico: %d\n", pontoTuristico);
-printf ("Cidade:  %s \n", nome);
-printf ("Area Territorial: %.2f KM² \n", area);
-printf ("Digite o PIB: %.2f  \n",pib);
-printf ("População: %.d  \n ", populacao);
-printf ("Desindade Populacional: %.2f\n", DensidadePopulacional);
-printf ("Pib Percapita: %f %\n", PibPercapita);
+
+    // Exibindo as informações da primeira carta
+    printf("\n----- Informações da primeira carta -----\n");
+    printf("Código da carta 1: %d\n", codigoDaCarta1);
+    printf("Cidade: %s\n", nome1);
+    printf("Área Territorial: %.2f KM²\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("População: %d\n", populacao1);
+    printf("Densidade Populacional: %.2f habitantes por KM²\n", DensidadePopulacional1);
+    printf("PIB Per Capita: %.2f\n", PibPercapita1);
+
+    // Exibindo as informações da segunda carta
+    printf("\n----- Informações da segunda carta -----\n");
+    printf("Código da carta 2: %d\n", codigoDaCarta2);
+    printf("Cidade: %s\n", nome2);
+    printf("Área Territorial: %.2f KM²\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("População: %d\n", populacao2);
+    printf("Densidade Populacional: %.2f habitantes por KM²\n", DensidadePopulacional2);
+    printf("PIB Per Capita: %.2f\n", PibPercapita2);
+
 
 return 0;
 
